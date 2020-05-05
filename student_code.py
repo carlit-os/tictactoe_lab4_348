9dfeff49a8ac1bbe445e8537c98ed827b724c32c
Andy Moran <49261838+carlit-os@users.noreply.github.com>
2020-05-05 00:34:53 -0500

    game_stats now works as an object Added turn and board attributes with a board mutator
    Also fixed detached head

--- student_code.py
+++ student_code.py
@@ -9,17 +9,27 @@ def is_tie(board):
 
 
 class game_stats:
-    def __init__(self):
+    def __init__(self, board, turn):
         self.outcome = None
+        self.turn = turn
+        self.board = board
 
-    def is_terminal(self, board):
-        status = common.game_status(board)
+    def is_terminal(self):
+        status = common.game_status(self.board)
         # 1 if X wins, 2 if O wins, 0 else
-        if status != 0 or is_tie(board):
+        if status != 0 or is_tie(self.board):
             self.outcome = status
             return True
         else:
             return False
+
+    def set_board(self, fresh_board):
+        self.board = fresh_board
+
+def agentX(stats):
+    v = float("-inf")
+    pass
+    #while not stats.is_terminal()
 
 
 def minmax_tictactoe(board, turn):
@@ -27,9 +37,15 @@ def minmax_tictactoe(board, turn):
     # tie. use the function common.game_status(board), to evaluate a board it returns common.constants.X(1) if X wins,
     # common.constants.O(2) if O wins or common.constants.NONE(0) if tie or game is not finished the program will keep
     # track of the number of boards evaluated result = common.game_status(board);
-    minmax_stats = game_stats()
-    while not minmax_stats.is_terminal(board):
-        pass
+    minmax_stats = game_stats(board, turn)
+
+    while not minmax_stats.is_terminal():
+        if turn == 1:
+            pass
+            #agentX(minmax_stats)
+        else:
+            pass
+            #agentY(minmax_stats)
 
     return common.constants.X
 
--- test_student_code.py
+++ test_student_code.py
@@ -6,24 +6,36 @@ from student_code import game_stats
 class Testgame_stats(TestCase):
 
     def test_full_boards(self):
-        samp_stats = game_stats()
-        self.assertFalse(samp_stats.is_terminal([0 for x in range(9)]))
-        self.assertTrue(samp_stats.is_terminal([1 for x in range(9)]))
-        self.assertTrue(samp_stats.is_terminal([2 for x in range(9)]))
+        samp_stats = game_stats([0 for x in range(9)], 1)
+        self.assertFalse(samp_stats.is_terminal())
+
+        samp_stats.set_board([1 for x in range(9)])
+        self.assertTrue(samp_stats.is_terminal())
+
+        samp_stats.set_board([2 for x in range(9)])
+        self.assertTrue(samp_stats.is_terminal())
 
     def test_ties(self):
-        samp_stats = game_stats()
-        self.assertTrue(samp_stats.is_terminal([1, 2, 1, 2, 1, 1, 2, 1, 2]))
-        self.assertTrue(samp_stats.is_terminal([2, 2, 1, 1, 1, 2, 2, 1, 2]))
+        samp_stats = game_stats([1, 2, 1, 2, 1, 1, 2, 1, 2], 1)
+        self.assertTrue(samp_stats.is_terminal())
+
+        samp_stats.set_board([2, 2, 1, 1, 1, 2, 2, 1, 2])
+        self.assertTrue(samp_stats.is_terminal())
 
     def test_incompletes(self):
-        samp_stats = game_stats()
-        self.assertFalse(samp_stats.is_terminal([1, 0, 1, 0, 0, 0, 0, 0, 2]))
-        self.assertFalse(samp_stats.is_terminal([0, 0, 0, 0, 0, 0, 0, 0, 2]))
+        samp_stats = game_stats([1, 0, 1, 0, 0, 0, 0, 0, 2], 1)
+        self.assertFalse(samp_stats.is_terminal())
+
+        samp_stats.set_board([0, 0, 0, 0, 0, 0, 0, 0, 2])
+        self.assertFalse(samp_stats.is_terminal())
 
     def test_quick_dubs(self):
-        samp_stats = game_stats()
-        self.assertTrue(samp_stats.is_terminal([1, 1, 1, 0, 0, 2, 0, 0, 2]))
-        self.assertTrue(samp_stats.is_terminal([2, 2, 2, 0, 0, 1, 0, 0, 1]))
-        self.assertTrue(samp_stats.is_terminal([2, 0, 0, 2, 0, 1, 2, 0, 1]))
+        samp_stats = game_stats([1, 1, 1, 0, 0, 2, 0, 0, 2], 1)
+        self.assertTrue(samp_stats.is_terminal())
+
+        samp_stats.set_board([2, 2, 2, 0, 0, 1, 0, 0, 1])
+        self.assertTrue(samp_stats.is_terminal())
+
+        samp_stats.set_board([2, 0, 0, 2, 0, 1, 2, 0, 1])
+        self.assertTrue(samp_stats.is_terminal())
 
