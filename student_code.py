db4d616abb0cd650f0fb452204c79c95807c90e1
Andy Moran <49261838+carlit-os@users.noreply.github.com>
2020-05-04 23:15:31 -0500

    Created class for game_stats and converted tests to reflect this.
    There is oddly no longer any output though.

--- student_code.py
+++ student_code.py
@@ -8,13 +8,18 @@ def is_tie(board):
     return True
 
 
-def is_terminal(board):
-    status = common.game_status(board)
-    # 1 if X wins, 2 if O wins, 0 else
-    if status != 0 or is_tie(board):
-        return True
-    else:
-        return False
+class game_stats:
+    def __init__(self):
+        self.outcome = None
+
+    def is_terminal(self, board):
+        status = common.game_status(board)
+        # 1 if X wins, 2 if O wins, 0 else
+        if status != 0 or is_tie(board):
+            self.outcome = status
+            return True
+        else:
+            return False
 
 
 def minmax_tictactoe(board, turn):
@@ -22,11 +27,11 @@ def minmax_tictactoe(board, turn):
     # tie. use the function common.game_status(board), to evaluate a board it returns common.constants.X(1) if X wins,
     # common.constants.O(2) if O wins or common.constants.NONE(0) if tie or game is not finished the program will keep
     # track of the number of boards evaluated result = common.game_status(board);
-
-    while not is_terminal(board):
+    minmax_stats = game_stats()
+    while not minmax_stats.is_terminal(board):
         pass
 
-    return common.constants.NONE
+    return common.constants.X
 
 
 def abprun_tictactoe(board, turn):
@@ -35,4 +40,3 @@ def abprun_tictactoe(board, turn):
     # common.constants.O(2) if O wins or common.constants.NONE(0) if tie or game is not finished the program will keep
     # track of the number of boards evaluated result = common.game_status(board);
     return common.constants.NONE
-
--- test_student_code.py
+++ test_student_code.py
@@ -1,27 +1,29 @@
 from unittest import TestCase
 
+from student_code import game_stats
 
-class TerminalTests(TestCase):
+
+class Testgame_stats(TestCase):
 
     def test_full_boards(self):
-        from student_code import is_terminal
-        self.assertFalse(is_terminal([0 for x in range(9)]))
-        self.assertTrue(is_terminal([1 for x in range(9)]))
-        self.assertTrue(is_terminal([2 for x in range(9)]))
+        samp_stats = game_stats()
+        self.assertFalse(samp_stats.is_terminal([0 for x in range(9)]))
+        self.assertTrue(samp_stats.is_terminal([1 for x in range(9)]))
+        self.assertTrue(samp_stats.is_terminal([2 for x in range(9)]))
 
     def test_ties(self):
-        from student_code import is_terminal
-        self.assertTrue(is_terminal([1, 2, 1, 2, 1, 1, 2, 1, 2]))
-        self.assertTrue(is_terminal([2, 2, 1, 1, 1, 2, 2, 1, 2]))
+        samp_stats = game_stats()
+        self.assertTrue(samp_stats.is_terminal([1, 2, 1, 2, 1, 1, 2, 1, 2]))
+        self.assertTrue(samp_stats.is_terminal([2, 2, 1, 1, 1, 2, 2, 1, 2]))
 
     def test_incompletes(self):
-        from student_code import is_terminal
-        self.assertFalse(is_terminal([1, 0, 1, 0, 0, 0, 0, 0, 2]))
-        self.assertFalse(is_terminal([0, 0, 0, 0, 0, 0, 0, 0, 2]))
+        samp_stats = game_stats()
+        self.assertFalse(samp_stats.is_terminal([1, 0, 1, 0, 0, 0, 0, 0, 2]))
+        self.assertFalse(samp_stats.is_terminal([0, 0, 0, 0, 0, 0, 0, 0, 2]))
 
     def test_quick_dubs(self):
-        from student_code import is_terminal
-        self.assertTrue(is_terminal([1, 1, 1, 0, 0, 2, 0, 0, 2]))
-        self.assertTrue(is_terminal([2, 2, 2, 0, 0, 1, 0, 0, 1]))
-        self.assertTrue(is_terminal([2, 0, 0, 2, 0, 1, 2, 0, 1]))
+        samp_stats = game_stats()
+        self.assertTrue(samp_stats.is_terminal([1, 1, 1, 0, 0, 2, 0, 0, 2]))
+        self.assertTrue(samp_stats.is_terminal([2, 2, 2, 0, 0, 1, 0, 0, 1]))
+        self.assertTrue(samp_stats.is_terminal([2, 0, 0, 2, 0, 1, 2, 0, 1]))
 
