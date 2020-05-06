09b3ec96c9d3c4825a1f48971e6f45a6a97b4315
Andy Moran <49261838+carlit-os@users.noreply.github.com>
2020-05-05 19:27:31 -0500

    Boards that have a winner and small depth now work
    The way functions return v currently seems to be an issue

--- student_code.py
+++ student_code.py
@@ -8,28 +8,100 @@ def is_tie(board):
     return True
 
 
+def possible_moves(board, player):
+    blanks = [idx for idx, box in enumerate(board) if box == 0]
+    result = []
+
+    for spot in blanks:
+        temp = board.copy()
+        temp[spot] = player
+        result.append(temp)
+
+    return result
+
+
 class game_stats:
     def __init__(self, board, turn):
-        self.outcome = None
+        self.outcome = 0
         self.turn = turn
         self.board = board
 
     def is_terminal(self):
         status = common.game_status(self.board)
         # 1 if X wins, 2 if O wins, 0 else
-        if status != 0 or is_tie(self.board):
+        if is_tie(self.board) or status != 0:
             self.outcome = status
             return True
         else:
             return False
 
+    def set_outcome(self):
+        self.outcome = common.game_status(self.board)
+
+    def get_board(self):
+        return self.board
+
+    def get_turn(self):
+        return self.turn
+
+    def get_outcome(self):
+        return self.outcome
+
+    def set_turn(self, fresh_turn):
+        self.turn = fresh_turn
+
     def set_board(self, fresh_board):
         self.board = fresh_board
 
-def agentX(stats):
-    v = float("-inf")
+
+def closer_to1(outcomes):
+    if 1 in outcomes:
+        return 1
+    elif 0 in outcomes:
+        return 0
+    elif 2 in outcomes:
+        return 2
+    else:
+        return float("inf")
     pass
-    #while not stats.is_terminal()
+
+
+def closer_to2(outcomes):
+    if 2 in outcomes:
+        return 2
+    elif 0 in outcomes:
+        return 0
+    elif 1 in outcomes:
+        return 1
+    else:
+        return float("-inf")
+    pass
+
+
+def agentX(curr_board, stats):
+    v = float("inf")
+    next_moves = possible_moves(curr_board, 1)
+
+    for state in next_moves:
+        stats.set_board(state)
+        if stats.is_terminal():
+            break
+        v = closer_to1([v, agentO(state, stats)])
+
+    return v
+
+
+def agentO(curr_board, stats):
+    v = float("-inf")
+    next_moves = possible_moves(curr_board, 2)
+
+    for state in next_moves:
+        stats.set_board(state)
+        if stats.is_terminal():
+            break
+        v = closer_to2([v, agentX(state, stats)])
+
+    return v
 
 
 def minmax_tictactoe(board, turn):
@@ -39,15 +111,15 @@ def minmax_tictactoe(board, turn):
     # track of the number of boards evaluated result = common.game_status(board);
     minmax_stats = game_stats(board, turn)
 
-    while not minmax_stats.is_terminal():
-        if turn == 1:
-            pass
-            #agentX(minmax_stats)
-        else:
-            pass
-            #agentY(minmax_stats)
+    if minmax_stats.is_terminal():
+        return minmax_stats.get_outcome()
 
-    return common.constants.X
+    if turn == 1:  # x goes first
+        agentX(minmax_stats.get_board(), minmax_stats)
+    else:  # o goes first
+        agentO(minmax_stats.get_board(), minmax_stats)
+
+    return minmax_stats.get_outcome()
 
 
 def abprun_tictactoe(board, turn):
--- test_student_code.py
+++ test_student_code.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from student_code import game_stats
+from student_code import game_stats, possible_moves
 
 
 class Testgame_stats(TestCase):
@@ -39,3 +39,20 @@ class Testgame_stats(TestCase):
         samp_stats.set_board([2, 0, 0, 2, 0, 1, 2, 0, 1])
         self.assertTrue(samp_stats.is_terminal())
 
+    def test_get_turn(self):
+        samp_stats = game_stats([1, 1, 1, 0, 0, 2, 0, 0, 2], 1)
+        self.assertTrue(samp_stats.get_turn() == 1)
+        samp_stats.set_turn(2)
+        self.assertTrue(samp_stats.get_turn() == 2)
+
+
+class Test(TestCase):
+    def test_possible_moves(self):
+        self.assertTrue([] == possible_moves([1, 2, 1, 2, 1, 1, 2, 1, 2], 1))
+
+        self.assertTrue([[1, 2, 1, 2, 1, 1, 2, 1, 1]] == possible_moves([1, 2, 1, 2, 1, 1, 2, 1, 0], 1))
+        self.assertTrue([[1, 2, 1, 2, 1, 1, 2, 1, 2]] == possible_moves([1, 2, 1, 2, 1, 1, 2, 1, 0], 2))
+
+        self.assertTrue(
+            [[1, 2, 1, 2, 1, 1, 2, 1, 0], [0, 2, 1, 2, 1, 1, 2, 1, 1]] == possible_moves([0, 2, 1, 2, 1, 1, 2, 1, 0],
+                                                                                         1))
